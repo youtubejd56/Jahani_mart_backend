df56d811dfecf9372d43e367e94130dc65d3003c
vinayak <kkvinayak718.com>
2026-03-22 19:12:52 +0530

    Disable strict whitenoise manifest

--- app/settings.py
+++ app/settings.py
@@ -161,6 +161,8 @@ STORAGES = {
     },
 }
 
+WHITENOISE_MANIFEST_STRICT = False
+
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
